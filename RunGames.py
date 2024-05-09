c0769063a277b397a3e31ca3fe8ddc45a27d5f87
Ivo Pačnik <pacnik@gmail.com>
2024-05-09 23:10:43 +0200

    Added plotting of results

--- RunGames.py
+++ RunGames.py
@@ -1,4 +1,5 @@
 from TurtleSim import *
+import matplotlib.pyplot as plt
 
 twoPlayerDeck = {
     #GreenwashValue:Quantity of cards
@@ -10,30 +11,51 @@ twoPlayerDeck = {
     5:2
 }
 
-#Count for the game
+#Rules for the game
 startingHandSize = 5
 turnsToTake = 5
 
-#Prepare Deck
-DeckList = TurnDeckIntoListOfCards(twoPlayerDeck)
+#Simulation run size
+simRunSize = 10000
 
-#Initialize game state
-CurrentGame = GameState()
-PlayerHand = CurrentGame.Hand
+greenWashCounts = {}
 
-#Prepare Starting Hand
-for _ in range(startingHandSize):
-    PlayerHand.Draw(DrawRandomCard(DeckList))
+for _ in range(simRunSize):
+    #Prepare Deck
+    DeckList = TurnDeckIntoListOfCards(twoPlayerDeck)
 
-#Play the 2 highest greenwash cards:
-CurrentGame.playHighestGreenWash()
-CurrentGame.playHighestGreenWash()
+    #Initialize game state
+    CurrentGame = GameState()
+    PlayerHand = CurrentGame.Hand
 
-#Plays out a number of turns specified in turnsToTake
-for _ in range(turnsToTake):
-    cardDrawn = DrawRandomCard(DeckList)
-    PlayerHand.Draw(cardDrawn)
-    CurrentGame.PlaySafe()
+    #Prepare Starting Hand
+    for _ in range(startingHandSize):
+        PlayerHand.Draw(DrawRandomCard(DeckList))
 
-#Prints result of game
-print(CurrentGame.GreenWash,CurrentGame.Industry)
+    #Play the 2 highest greenwash cards:
+    CurrentGame.playHighestGreenWash()
+    CurrentGame.playHighestGreenWash()
+
+    #Plays out a number of turns specified in turnsToTake
+    for _ in range(turnsToTake):
+        cardDrawn = DrawRandomCard(DeckList)
+        PlayerHand.Draw(cardDrawn)
+        CurrentGame.PlaySafe()
+
+    endGameScore = CurrentGame.GreenWash
+
+    if endGameScore in greenWashCounts:
+        greenWashCounts[endGameScore] += 1
+    else:
+        greenWashCounts[endGameScore] = 1
+
+GreenWashScores = list(greenWashCounts.keys())
+GreenWashGames = list(greenWashCounts.values())
+
+plt.bar(GreenWashScores, GreenWashGames)
+
+plt.title("Results after playing "+str(simRunSize)+" games")
+plt.xlabel("End of game GreenWash value")
+plt.ylabel("Number of games with that score")
+
+plt.show()
